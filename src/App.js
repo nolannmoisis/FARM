a1e872d1db5ae9d7ea45a0fa142b745fa6d2204a
nolannmoisis <moisis@et.esiea.fr>
2025-04-22 17:45:06 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
-import ListToDoLists from "./ListToDoLists";
+import ListToDoLists from "./ListTodoLists";
 import ToDoList from "./ToDoList";
 
 function App() {
